99fe5e5a288588bdd7ae1275f461c1a35a6ba914
FrancoCorporation <rodolfofrancoxico@gmail.com>
2026-09-20 22:56:49 -0300

    fix: porta e MONGO_URI via env, erro amigavel de porta em uso e conexao mongo tratada

--- src/start.js
+++ src/start.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 require('./controller/authController')(app)
 
-app.listen(port,() => {
+const server = app.listen(port, () => {
   console.log("Listeing in http://localhost:"+port)
 })
